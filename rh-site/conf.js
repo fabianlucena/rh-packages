76749656cb026c34498abfce05f192570b414b55
Fabian Lucena <fabianlucena@gmail.com>
2023-04-24 13:16:53 -0300

    RH Site: fix some permission to global.

--- rh-site/conf.js
+++ rh-site/conf.js
@@ -23,9 +23,9 @@ export const conf = {
         },
 
         permissions: {
-            'current-site.switch': {title: l._f('Switch site'),      type: 'private', roles: 'user', ownerModule: name, menuItem: {service: 'site',  action: 'form'}},
-            'current-site.get':    {title: l._f('Get current site'), type: 'private', roles: 'user', ownerModule: name, menuItem: {service: 'site',  action: 'get'}},
-            'site.get':            {title: l._f('Get site(s)'),      type: 'private', roles: 'user', ownerModule: name, menuItem: {service: 'site',  action: 'form'}},
+            'current-site.switch': {title: l._f('Switch site'),      type: 'global', ownerModule: name, menuItem: {service: 'site',  action: 'form'}},
+            'current-site.get':    {title: l._f('Get current site'), type: 'global', ownerModule: name, menuItem: {service: 'site',  action: 'get'}},
+            'site.get':            {title: l._f('Get site(s)'),      type: 'global', ownerModule: name, menuItem: {service: 'site',  action: 'form'}},
         },
     },
 };
