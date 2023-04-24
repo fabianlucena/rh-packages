af040d546f37a6c8d915b210d8b98b51da3b3744
Fabian Lucena <fabianlucena@gmail.com>
2023-04-24 12:36:09 -0300

    RH Auth, add CORS support to user and session endpoints.

--- rh-auth/routes/session.js
+++ rh-auth/routes/session.js
@@ -1,8 +1,8 @@
 import {SessionController} from '../controllers/session.js';
-import {methodNotAllowed, asyncHandler} from 'http-util';
+import {methodNotAllowed, corsSimplePreflight, asyncHandler} from 'http-util';
 
 export default (app, checkPermission) => {
-    app.head('/session', methodNotAllowed);
+    app.options('/session', corsSimplePreflight('GET,HEAD,DELETE'));
     app.get('/session', checkPermission('ownsession.get', 'session.get'), asyncHandler(SessionController.get));
     app.delete('/session', checkPermission('session.delete'), asyncHandler(SessionController.delete));
     app.all('/session', methodNotAllowed);
--- rh-auth/routes/user.js
+++ rh-auth/routes/user.js
@@ -1,18 +1,30 @@
 import {UserController} from '../controllers/user.js';
-import {methodNotAllowed, asyncHandler} from 'http-util';
+import {methodNotAllowed, corsSimplePreflight, asyncHandler} from 'http-util';
 
 export default (app, checkPermission) => {
-    app.head('/user', methodNotAllowed);
+    app.options('/user', corsSimplePreflight('GET,HEAD,POST,PATCH,DELETE'));
+    app.options('/user/enable', corsSimplePreflight('POST'));
+    app.options('/user/enable/:uuid', corsSimplePreflight('POST'));
+    app.options('/user/disable', corsSimplePreflight('POST'));
+    app.options('/user/disable/:uuid', corsSimplePreflight('POST'));
+    app.options('/user/:uuid', corsSimplePreflight('GET,PATCH,DELETE'));
+
     app.post('/user', checkPermission('user.create'), asyncHandler(UserController.post));
     app.get('/user', checkPermission('user.get'), asyncHandler(UserController.get));
+
     app.get('/user/:uuid', checkPermission('user.get'), asyncHandler(UserController.get));
+
     app.delete('/user', checkPermission('user.delete'), asyncHandler(UserController.delete));
-    app.delete('/user/:uuid', checkPermission('user.get'), asyncHandler(UserController.delete));
+    app.delete('/user/:uuid', checkPermission('user.delete'), asyncHandler(UserController.delete));
+
     app.post('/user/enable', checkPermission('user.update'), asyncHandler(UserController.enablePost));
     app.post('/user/enable/:uuid', checkPermission('user.update'), asyncHandler(UserController.enablePost));
+    
     app.post('/user/disable', checkPermission('user.update'), asyncHandler(UserController.disablePost));
     app.post('/user/disable/:uuid', checkPermission('user.update'), asyncHandler(UserController.disablePost));
+
     app.patch('/user', checkPermission('user.update'), asyncHandler(UserController.patch));
     app.patch('/user/:uuid', checkPermission('user.update'), asyncHandler(UserController.patch));
+
     app.all('/user', methodNotAllowed);
 };
