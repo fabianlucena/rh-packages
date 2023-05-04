39998836339d65d920f9f56a8edbb1d46c12a746
Fabian Lucena <fabianlucena@gmail.com>
2023-05-04 15:06:20 -0300

    RH Locale add texts to translate.

---
+++ rh-locale/texts.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import {loc} from 'rf-locale';
+
+loc._f('Create');
+loc._f('Search');
+loc._f('First');
+loc._f('Next');
+loc._f('Prior');
+loc._f('Last');
+loc._f('Delete');
+loc._f('Edit');
+loc._f('Enable');
+loc._f('Disable');
