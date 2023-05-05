b31ec74aaa6f528abef0b6e0f9b8883090c24925
Fabian Lucena <fabianlucena@gmail.com>
2023-05-05 13:31:18 -0300

    Update translations.

--- rh-locale/texts.js
+++ rh-locale/texts.js
@@ -2,13 +2,14 @@
 
 import {loc} from 'rf-locale';
 
+loc._f('Add');
 loc._f('Create');
-loc._f('Search');
-loc._f('First');
-loc._f('Next');
-loc._f('Prior');
-loc._f('Last');
 loc._f('Delete');
+loc._f('Disable');
 loc._f('Edit');
 loc._f('Enable');
-loc._f('Disable');
+loc._f('First');
+loc._f('Last');
+loc._f('Next');
+loc._f('Prior');
+loc._f('Search');
