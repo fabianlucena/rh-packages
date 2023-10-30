86714792507d038b08e30b52cf1aa39cebbc5165
Fabian Lucena <fabianlucena@gmail.com>
2023-10-30 12:45:32 -0300

    RH Page: add module support.

--- rh-page/models/page.js
+++ rh-page/models/page.js
@@ -3,8 +3,9 @@ import {conf} from '../conf.js';
 export default (sequelize, DataTypes) => {
     class Page extends sequelize.Sequelize.Model {
         static associate(models) {
-            this.belongsTo(models.Language,   {foreignKey: 'languageId', allowNull: true});
-            this.belongsTo(models.PageFormat, {as: 'Format', foreignKey: 'formatId',   allowNull: false});
+            this.belongsTo(models.Module,     {foreignKey: 'ownerModuleId', as: 'OwnerModule', allowNull: true});
+            this.belongsTo(models.Language,   {foreignKey: 'languageId',    as: 'Format',      allowNull: true});
+            this.belongsTo(models.PageFormat, {foreignKey: 'formatId',                         allowNull: false});
         }
 
         static postAssociate(models) {
