7dad01fcdf55d697ebef4b682c57d1eaa610cc75
Fabian Lucena <fabianlucena@gmail.com>
2023-10-30 11:56:52 -0300

    RH Auth: UserTypeService class generalization.

--- rh-auth/services/user_type.js
+++ rh-auth/services/user_type.js
@@ -1,46 +1,9 @@
-'use strict';
-
 import {conf} from '../conf.js';
-import {ServiceIdUuidNameEnableTranslatable} from 'rf-service';
-import {addEnabledFilter, addEnabledOnerModuleFilter, checkDataForMissingProperties} from 'sql-util';
+import {ServiceIdUuidNameTitleEnabledModuleTranslatable} from 'rf-service';
 
-export class UserTypeService extends ServiceIdUuidNameEnableTranslatable {
+export class UserTypeService extends ServiceIdUuidNameTitleEnabledModuleTranslatable {
     sequelize = conf.global.sequelize;
+    ModuleService = conf.global.models.Module;
     model = conf.global.models.UserType;
     defaultTranslationContext = 'userType';
-
-    async validateForCreation(data) {
-        await checkDataForMissingProperties(data, 'UserType', 'name', 'title');
-
-        return true;
-    }
-
-    /**
-     * Gets the options for use in the getList and getListAndCount methods.
-     * @param {Options} options - options for the @see sequelize.findAll method.
-     *  - view: show visible peoperties.
-     * @returns {options}
-     */
-    async getListOptions(options) {
-        options ??= {};
-
-        if (options.q) {
-            const q = `%${options.q}%`;
-            const Op = conf.global.Sequelize.Op;
-            options.where = {
-                [Op.or]: [
-                    {username:    {[Op.like]: q}},
-                    {displayName: {[Op.like]: q}},
-                ],
-            };
-        }
-
-        if (options.isEnabled !== undefined) {
-            options = addEnabledFilter(options);
-            if (conf.global.models.Module)
-                options = addEnabledOnerModuleFilter(options, conf.global.models.Module);
-        }
-
-        return options;
-    }
 }
