1e223d832186a7c9cbd61f49ac76c9de5f99d96d
Fabian Lucena <fabianlucena@gmail.com>
2024-03-09 21:26:57 -0300

    Add RF Get unique filename package.

---
+++ rf-get-unique-filename/rf-get-unique-filename.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
+export function getUniqueFilename(filename) {
+    if (!fs.existsSync(filename)) {
+        return filename;
+    }
+
+    const dirname = path.dirname(filename);
+    const extname = path.extname(filename);
+    const basename = path.basename(filename, extname) + '-';
+    
+    let c = 0;
+    /*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
+    while (true) {
+        let tryFilename = path.join(dirname, basename + (new String(c)).padStart(6, '0') + extname);
+        if (!fs.existsSync(tryFilename)) {
+            if (filename.startsWith('./')) {
+                tryFilename = './' + tryFilename;
+            } else if (filename.startsWith('.\\')) {
+                tryFilename = '.\\' + tryFilename;
+            }
+
+            return tryFilename;
+        }
+
+        c++;
+    }
+}
