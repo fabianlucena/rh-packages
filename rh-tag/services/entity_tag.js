72c5fd02d8145ff3889480647a56ab19f726233f
Fabian Lucena <fabianlucena@gmail.com>
2024-03-10 22:51:55 -0300

    RH Tag: depends from RH language.

--- rh-tag/services/entity_tag.js
+++ rh-tag/services/entity_tag.js
@@ -238,20 +238,12 @@ export class EntityTagService extends ServiceBase {
         }
 
         result = await result;
-        let rows = options.withCount? result.rows: result;
-
-        for (const i in rows) {
-            const row = rows[i];
+        for (const i in result) {
+            const row = result[i];
             const tags = await this.getForEntityId(row.id);
             row.tags = tags.map(tag => tag.Tag.name);
 
-            rows[i] = row;
-        }
-
-        if (options.withCount) {
-            result.rows = rows;
-        } else {
-            result = rows;
+            result[i] = row;
         }
 
         return result;
