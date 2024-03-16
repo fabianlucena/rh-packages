57b51f6b4c49225360c0dfd3e8176fbb926ba159
Fabian Lucena <fabianlucena@gmail.com>
2024-03-16 20:13:21 -0300

    RH Progress add the package.

---
+++ rh-progress/rh-progress.js
@@ -0,0 +1,118 @@
+export class Progress {
+    separator = '\n';
+    progressJsonPropertyName = 'progress';
+    progressMessagePropertyName = 'message';
+
+    constructor(res, options) {
+        options = {...options};
+        if (res.constructor.name === 'ServerResponse') {
+            options.res = res;
+        } else {
+            options = {...res, ...options};
+        }
+
+        this.setOptions(options);
+    }
+
+    setOptions(options) {
+        for (const k in options) {
+            this[k] = options[k];
+        }
+    }
+
+    async init(options) {
+        this.setOptions(options);
+
+        if (typeof this['Content-Type'] === 'string') {
+            this.res.setHeader('Content-Type', this['Content-Type']);
+        } else if (this['Content-Type'] !== false) {
+            if (this.json) {
+                this.res.setHeader('Content-Type', 'application/json');
+            } else {
+                this.res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+            }
+        }
+
+        if (this['Content-Type'] !== false) {
+            this.res.setHeader('Transfer-Encoding', this['Transfer-Encoding'] ?? 'chunked');
+        }
+
+        if (this.json) {
+            this.res.write('{');
+            if (this.progressJsonPropertyName) {
+                this.res.write(`"${this.progressJsonPropertyName}":0`);
+            }
+        }
+    }
+
+    async end(data) {
+        this.sendProgress(1, data);
+        if (this.json) {
+            this.res.write('}');
+        }
+        
+        this.res.end();
+    }
+
+    async sendRaw(data) {
+        if (data === undefined) {
+            return;
+        }
+
+        if (typeof data === 'object') {
+            data = JSON.stringify(data);
+        }
+
+        if (this.separator) {
+            data = this.separator + data;
+        }
+
+        this.res.write(data);
+    }
+
+    async send(data) {
+        if (data === undefined) {
+            return;
+        }
+
+        if (this.json) {
+            if (typeof data !== 'string') {
+                data = JSON.stringify(data);
+            }
+
+            data = `,${data.slice(1, -1)}`;
+        }
+
+        this.sendRaw(data);
+    }
+
+    async sendProgress(progress, data) {
+        let progressData;
+        if (this.onProgress) {
+            progressData = this.onProgress(progress);
+        }
+
+        if (this.json) {
+            data = {...data};
+            if (this.progressJsonPropertyName) {
+                data[this.progressJsonPropertyName] = progress;
+            }
+
+            if (progressData) {
+                if (typeof progressData !== 'object' && this.progressMessagePropertyName) {
+                    progressData = {[this.progressMessagePropertyName]: progressData};
+                }
+
+                if (typeof progressData === 'object') {
+                    data = {...data, ...progressData};
+                }
+            }
+
+            this.send(data);
+        } else {
+            this.send(progress.toString());
+            this.send(progressData);
+            this.send(data);
+        }
+    }
+}
