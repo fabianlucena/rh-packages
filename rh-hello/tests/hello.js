ef03bbbaa98b9e4a318c913faf943c33c5058109
Fabian Lucena <fabianlucena@gmail.com>
2024-05-03 14:15:06 -0300

    RH Hello: add test.

---
+++ rh-hello/tests/hello.js
@@ -0,0 +1,23 @@
+import { rt } from 'rh-test';
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Hello', () => {
+  before(function () {
+    if (!rt.hasModule('rhHello')) {
+      this.skip();
+    }
+  });
+
+  it('Must return Hello World!', (done) => {
+    let credentials = {};
+    rt.getAgent()
+      .get(rt.base + '/hello')
+      .send(credentials)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
