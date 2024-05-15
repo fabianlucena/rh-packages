59996524be97d7cc4808e09cbbd8e45e7e1a2b94
Fabian Lucena <fabianlucena@gmail.com>
2024-05-15 18:55:31 -0300

    Eslint configuration update.

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,17 @@ import pluginJs from '@eslint/js';
 export default [
   {
     languageOptions: {
-      globals: globals.browser
-    }
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
   },
   pluginJs.configs.all,
   mochaPlugin.configs.flat.all,
   {
     rules: {
+      'class-methods-use-this': 'off',
       'func-style': 'off',
       'indent': ['error', 2],
       'linebreak-style': ['error', 'windows'],
@@ -21,10 +25,15 @@ export default [
       'no-ternary': 'off',
       'no-undef': 'error',
       'no-undef-init': 'error',
+      'no-underscore-dangle': 'off',
       'no-unused-vars': 'error',
       'object-curly-spacing': ['error', 'always', { 'objectsInObjects': false }],
+      'one-var': 'off',
       'quotes': ['error', 'single', { 'avoidEscape': true }],
-      'semi': ['error', 'always']
+      'semi': ['error', 'always'],
+      'sort-imports': 'off',
+      'sort-keys': 'off',
+      'sort-vars': 'off',
     }
   }
 ];
