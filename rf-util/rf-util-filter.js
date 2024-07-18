1323f3e74353e715b771965af5cc854acdcedd90
Fabian Lucena <fabianlucena@gmail.com>
2024-07-18 20:04:02 -0300

    RF Util: filterVisualItemsByAliasName for use interface.

--- rf-util/rf-util-filter.js
+++ rf-util/rf-util-filter.js
@@ -1,12 +1,15 @@
 import { defaultLoc } from 'rf-locale';
+import { ucfirst } from './rf-util-string.js';
 
-export async function filterVisualItemsByAliasName(items, filter, options) {
+export async function filterVisualItemsByAliasName(items, options) {
   const loc = options?.loc ?? defaultLoc;
   const translationContext = options?.translationContext;
 
-  const filtered = [];
+  const filtered = [],
+    filter = options?.filter,
+    interfaceName = options?.interface;
   for (const item of items) {
-    let hideValue = filter?.hide?.[item.alias ?? item.name];
+    let hideValue = filter?.hide?.[item.name];
     if (typeof hideValue === 'function') {
       hideValue = await hideValue(options);
     }
@@ -14,12 +17,36 @@ export async function filterVisualItemsByAliasName(items, filter, options) {
     if (hideValue) {
       continue;
     }
+
+    if (interfaceName) {
+      const substitutions = [
+        'label',
+        'title',
+        'placeholder',
+        'type',
+        'name',
+      ];
+      for (const dst in substitutions) {
+        const src = interfaceName + ucfirst(dst);
+        if (item[src] === undefined) {
+          continue;
+        }
+
+        item[dst] = item[src];
+        delete item[src];
+      }
+    }
             
-    if (loc) {
-      const thisTranslationContext = item.translationContext ?? translationContext;
-      if (item.label)       item.label =       await loc._c(thisTranslationContext, item.label);
-      if (item.title)       item.title =       await loc._c(thisTranslationContext, item.title);
-      if (item.placeholder) item.placeholder = await loc._c(thisTranslationContext, item.placeholder);
+    if (typeof item.label === 'function') {
+      item.label = await item.label(loc, translationContext);
+    }
+
+    if (typeof item.label === 'function') {
+      item.title = await item.title(loc, translationContext);
+    }
+
+    if (typeof item.label === 'function') {
+      item.placeholder = await item.placeholder(loc, translationContext);
     }
 
     filtered.push(item);
